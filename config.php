fe0cd6a164a6a10033f012962886045509aa88d9
Aleixenandros <aleixenandros@gmail.com>
2025-02-05 01:27:38 +0100

    Se añaden datos demo

--- config.php
+++ config.php
@@ -3,14 +3,14 @@ return [
     // 'directory' => Lee archivos .cer de una carpeta
     // 'txt'       => Lee dominios de un archivo de texto y verifica el certificado remoto
     // Puede ser ['directory'], ['txt'] o ['directory', 'txt'] si quieres ambos
-    'read_mode' => ['directory'],
+    'read_mode' => ['directory', 'txt'],
 
     // Carpeta con archivos .cer (solo se usa si read_mode='directory')
     'cert_directory' => './certificados/',
 
     // Fichero de texto con un listado de dominios (uno por línea)
     // (solo se usa si read_mode='txt')
-    'domains_file' => './certificados/domains.txt',
+    'domains_file' => './certificados/dominios.txt',
     
     // Días para considerar "próxima expiración"
     'prox_expir' => 45,
